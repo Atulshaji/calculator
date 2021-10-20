1cd9113765345950750fac9db6d9fee77a5aa829
Athul Shaji <athulshaji34@gmail.com>
2021-10-20 09:39:53 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,18 @@
+// function displayValue(num){
+//     let res=document.querySelector("#result")
+//     res.value+=num
+// }
+
+// function clearBox(){
+//     result.value=""
+// }
+
+// function evaluateExpression(){
+//     let expr=res.value;
+//     let out=eval(result)
+//     result.value=out
+// }
+
+var displayValue=(num)=>result.value+=num
+var clearBox=()=>result.value=""
+var evaluateExpression=()=>result.value=eval(result.value)
